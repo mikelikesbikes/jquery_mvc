3c4acf098e8ed12058dae95a261cafdbbb0d50f0
Mike Busch <busch.michael@gmail.com>
2009-10-01 22:44:24 -0400

    tests for instantiating a view object

--- jquery.mvc.js
+++ jquery.mvc.js
@@ -1,47 +1,10 @@
 (function($) {
-  $.Template = function(str, options){
-
-    this.settings = $.extend({}, $.Template.defaults, options);
-    this.str = $.trim($('<div>').append($(str).clone()).remove().html()); // modify to take jquery, string, function
-
-    this.fill = function(params) {
-      regex = new RegExp(this.settings.begin_sep + "(.+?)" + this.settings.end_sep, "g");
-      return this.str.replace(regex,
-        function (m, key) {
-          var value = params[key];
-          return typeof value === 'string' || typeof value === 'number' ? value : "";
-      });
+  $.mvc = {
+    View : function(name, source, options){
+      this.settings = $.extend({}, $.mvc.View.defaults, options);
+      this.name = name;
+      this.template = (source instanceof $.Template)? source : new $.Template(source);
     }
-  }
-  $.Template.defaults = {
-    "begin_sep" : "__",
-    "end_sep": "__"
   };
-
-  (function(oldDomManip){
-    $.fn.domManip = function() {
-      var args = arguments[0];
-      if(args && args.length > 1 && args[0] instanceof $.Template) {
-        arguments[0] = [ args[0].fill(args[1]) ];
-      }
-
-      // Call the original method
-      return oldDomManip.apply(this, arguments);
-    }
-  })($.fn.domManip);
-
-  $.fn.templatize = function(params, options) {
-    $this = $(this);
-    var content = ($this.length > 0)? $this[0] : "";
-    var template = new $.Template(content, options);
-    return $(template.fill(params));
-  }
-
-  $.fn.outerHtml = function() {
-    var str = "";
-    if($(this) && $(this).length > 0) {
-      str = $.trim($('<div>').append($(this)[0]).remove().html());
-    }
-    return str;
-  }
+  $.mvc.View.defaults = {};
 })(jQuery);
